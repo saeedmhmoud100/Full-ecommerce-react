0209bdde8b29094a3ec5e01fb77be848a648e287
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-01-29 22:18:38 +0200

    was created User's add new Address Page design

---
+++ src/Components/User/UserAddAddress.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {Col, Row} from "react-bootstrap";
+
+function UserAddAddress() {
+
+    return (
+        <div>
+            <Row className="justify-content-start ">
+                <div className="admin-content-text pb-2">Add New Address</div>
+                <Col sm="8">
+                    <input
+                        type="text"
+                        className="input-form d-block mt-3 px-3"
+                        placeholder="Address labeling, for example (home - work)"
+                    />
+                    <textarea
+                        className="input-form-area p-2 mt-3"
+                        rows="4"
+                        cols="50"
+                        placeholder="Address in detail"
+                    />
+                    <input
+                        type="text"
+                        className="input-form d-block mt-3 px-3"
+                        placeholder="Phone Number"
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col sm="8" className="d-flex justify-content-between align-items-center ">
+                    <Link to={'/user/addresses'} className='text-decoration-none'>{'<<'}Back</Link>
+                    <button className="btn-save d-inline mt-2 ">Add</button>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default UserAddAddress
--- src/Routes/UserRouts.js
+++ src/Routes/UserRouts.js
@@ -2,12 +2,14 @@ import {Route, Routes} from "react-router-dom";
 import UserAllOrdersPage from "../Pages/User/UserAllOrdersPage";
 import UserFavoriteProductsPage from "../Pages/User/UserFavoriteProductsPage";
 import UserAllAddressesPage from "../Pages/User/UserAllAddressesPage";
+import UserAddAddressPage from "../Pages/User/UserAddAdressPage";
 
 const UserRoutes = () => ( // URL is: admin/*
     <Routes>
         <Route path={'allorders'} element={<UserAllOrdersPage />} />
         <Route path={'favoriteproducts'} element={<UserFavoriteProductsPage />} />
         <Route path={'addresses'} element={<UserAllAddressesPage />} />
+        <Route path={'add-address'} element={<UserAddAddressPage />} />
 
     </Routes>
 );
