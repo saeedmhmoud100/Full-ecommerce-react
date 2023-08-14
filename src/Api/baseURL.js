f4fa441ecb14fdf4191d42ba7a3a76e30d189a52
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-08-14 22:59:52 +0300

    return backend url to deploy

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = axios.create({baseURL:'https://react-full-ecommerce-api.onrender.com'})
-const baseURL = axios.create({baseURL:'http://localhost:8000'})
+const baseURL = axios.create({baseURL:'https://react-full-ecommerce-api.onrender.com'})
+// const baseURL = axios.create({baseURL:'http://localhost:8000'})
 
 
 
